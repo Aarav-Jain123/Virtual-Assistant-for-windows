35b9138d60e366d7e6d24296c1947ecc01715cd9
Aarav Jain <jainaarav552@gmail.com>
2023-12-31 23:58:34 +0530

    modified:   Indigo/settings.py 	modified:   Indigo/urls.py 	deleted:    authh/__init__.py 	deleted:    authh/admin.py 	deleted:    authh/apps.py 	deleted:    authh/migrations/__init__.py 	deleted:    authh/models.py 	deleted:    authh/tests.py 	deleted:    authh/views.py 	new file:   mainapp/urls.py 	modified:   mainapp/views.py 	modified:   templates/authenticate_user/login.html 	modified:   templates/authenticate_user/signup.html 	deleted:    templates/home&other/index.html 	renamed:    templates/home&other/base.html -> templates/homeother/base.html 	new file:   templates/homeother/index.html

--- Indigo/settings.py
+++ Indigo/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp.apps.MainappConfig',
-    'authh.apps.AuthhConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'Indigo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
