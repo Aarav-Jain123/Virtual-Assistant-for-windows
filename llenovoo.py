730c7a46b0a71edf70be3809d5c935da1ea503fd
Aarav Jain <jainaarav552@gmail.com>
2023-12-31 22:56:35 +0530

    modified:   llenovoo.py 	Indigo/ 	auth/ 	mainapp/ 	startvirtualassistant.bat

--- llenovoo.py
+++ llenovoo.py
@@ -1,11 +1,14 @@
 import listentouser
 import voicee
 import weatherr
+import os
 
 if __name__ == "__main__":
     voicee.speak('Greetings user,')
     weatherr.greet_user()
     q = listentouser.takeCommand()
-    if 'hello' in q:
+    if 'indigo' in q:
             print(f'Output: hello')
             voicee.speak(text="hello")
+    elif 'open code' in q:
+          os.startfile("C:\\Users\\Aarav Jain\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
