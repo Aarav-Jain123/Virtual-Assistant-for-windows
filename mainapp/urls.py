3f8747c20a01c598f3063b99e1c640e230891d88
Aarav Jain <jainaarav552@gmail.com>
2024-01-05 11:42:55 +0530

    modified:   Indigo/urls.py 	modified:   mainapp/urls.py 	modified:   mainapp/views.py 	modified:   manage.py 	deleted:    templates/authenticate_user/login.html 	deleted:    templates/authenticate_user/signup.html 	modified:   templates/homeother/base.html 	modified:   templates/homeother/index.html 	static/ 	templates/authenticateuser/

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('signup/', signup, name='Signup'),
+    path('login/', loginn, name='Login')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'homeother/index.html')
+
+def loginn(request):
+    return render(request, 'authenticateuser/login.html')
+
+
+def signup(request):
+    return render(request, 'authenticateuser/signup.html')
