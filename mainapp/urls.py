68e85fd649c0784687df9f62700add32fc49bd61
Aarav Jain <jainaarav552@gmail.com>
2024-01-08 11:10:08 +0530

    modified:   Indigo/urls.py 	modified:   mainapp/urls.py 	modified:   mainapp/views.py 	modified:   templates/authenticateuser/login.html 	new file:   templates/authenticateuser/otp.html 	modified:   templates/authenticateuser/signup.html

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('signup/', signup, name='Signup'),
     path('login/', loginn, name='Login'),
-    path('newspage/', indian_news_page, name='News')
+    # path('newspage/', indian_news_page, name='News'),
+    path('otp/', send_otp_to_user, name="OTP"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -17,3 +17,7 @@ def signup(request):
 
 def indian_news_page(request):
     return render(request, 'homeother/newspage.html', context=extractnews.main)
+
+
+def send_otp_to_user(request):
+    return render(request, 'authenticateuser/otp.html')
