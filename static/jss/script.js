5aefc6d9d98f2274dfb29c5f6db04d9e9e4830f5
Aarav Jain <jainaarav552@gmail.com>
2024-01-10 10:38:19 +0530

    new file:   static/csss/style.css 	new file:   static/jss/auth.js 	new file:   static/jss/script.js 	new file:   templates/authenticateuser/logout.html

---
+++ static/jss/script.js
@@ -0,0 +1,7 @@
+// function checkBasicAuthenticationRequirements(){
+//     let passwordd = document.
+//     let comfirmPassword = document.getElementById('comfirm-password')
+
+//     let passwordVal = passwordd.value
+//     let comfirmPasswordVal = comfirmPassword.value
+// }
